fabbc2e38e560e56e5ad1c855f7ea166d1b969ea
piskapratiwi28 <physkahrmn@gmail.com>
2025-03-17 15:04:22 +0700

    update

---
+++ script.js
@@ -0,0 +1,13 @@
+function learnMore() {
+    alert("Redirecting to more information Piska Pratiwi");
+}
+function downloadCV() {
+    alert("Downloading CV Piska Pratiwi");
+}
+
+
+  
+  
+  
+  
+  
